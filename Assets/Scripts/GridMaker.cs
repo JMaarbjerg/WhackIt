b11b779e3b633663d8f10b2678ae2d0d846d321c
JMAA <jacobmc@gmail.com>
2018-10-09 11:27:44 +0200

    Grid of spawner squares

---
+++ Assets/Scripts/GridMaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMaker : MonoBehaviour {
+
+	public GameObject spawner;
+    public GameObject[] clones;
+    public int sizeX = 10;
+    public int sizeY = 10;
+
+	// Use this for initialization
+	void Start () {
+        MakeGrid();
+	}
+	
+	// Update is called once per frame
+	void MakeGrid () {
+		for (int x = 0; x <= sizeX; x++) {
+            for (int y = 0; y <= sizeY; y++) {
+                Instantiate(spawner, new Vector3(x, 0, y), Quaternion.identity);
+            }			
+		}
+        clones = GameObject.FindGameObjectsWithTag("Spawner");
+
+	}
+}
